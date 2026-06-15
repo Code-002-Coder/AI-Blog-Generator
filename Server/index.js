861d8107562c25e7a531fdeb5845fb265a9cabb7
Avadh Agravat <avadhagravat297@gmail.com>
2026-06-15 15:07:19 +0530

    Add res line no 14

--- Server/index.js
+++ Server/index.js
@@ -11,6 +11,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" })   // gemini-2.5-flash-lite
 
 app.get('/', (req, res) => {
+  res.send("Server running ✅");
   //res.sendFile("index.html", { root: "D:\\AI Blog Generator\\blog-generator" });
 });
 
